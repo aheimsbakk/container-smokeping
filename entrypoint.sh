9a2b92d874e4e218b678516b6eee45d0a48dc654
aheimsbakk <arnulf@heimsbakk.net>
2020-06-08 16:46:44 +0200

    slave name defaults to short hostname

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 sed -Ei "s|^(MASTER=).*$|\1${MASTER}|g" /etc/default/smokeping; \
 sed -Ei "s|^..(MASTER_URL=).*$|\1${MASTER_URL}|g" /etc/default/smokeping; \
 sed -Ei "s|^..(SHARED_SECRET=).*$|\1${SHARED_SECRET}|g" /etc/default/smokeping; \
-sed -Ei "s|^..(SLAVE_NAME=).*$|\1${SLAVE_NAME}|g" /etc/default/smokeping
+sed -Ei "s|^..(SLAVE_NAME=).*$|\1${SLAVE_NAME:-$(hostname -s)}|g" /etc/default/smokeping
 
 rsync -ar --ignore-existing /srv/smokeping/ /etc/smokeping/
 
